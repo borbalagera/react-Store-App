435b8a971bbb917bad767866ee7e270517358bb7
borbalagera <borbalagera@gmail.com>
2020-08-05 21:10:52 +0200

    hashrouter changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <ProductProvider>
-    <Router>
+    <Router basename="/">
       <App />
     </Router>
   </ProductProvider>,
