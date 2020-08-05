7e3a9008be42320239f322f3a564f0806461c952
borbalagera <borbalagera@gmail.com>
2020-08-05 19:33:10 +0200

    Updates

---
+++ precache-manifest.88a392a3ac2f4e4337e344ed3ec89769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793cb6def27f3aa8b5cdcbadaf8392c4",
+    "url": "/react-Store-App/index.html"
+  },
+  {
+    "revision": "4e2eb3931a1baf24a45c",
+    "url": "/react-Store-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "776837a026f3c5833313",
+    "url": "/react-Store-App/static/js/2.99a4e108.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Store-App/static/js/2.99a4e108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2eb3931a1baf24a45c",
+    "url": "/react-Store-App/static/js/main.668ecaf4.chunk.js"
+  },
+  {
+    "revision": "c9a1fbbe4ca8eaca878c",
+    "url": "/react-Store-App/static/js/runtime-main.0f61be8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-Store-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
